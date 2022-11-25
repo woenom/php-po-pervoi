a70c8716eb1f26069b2d8bcf52c72d0c1ec34744
woenom <99072437+woenom@users.noreply.github.com>
2022-11-25 13:07:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div class="form-group">
+<h3>Вариант 16.</h3>
+<img src="img.jpg" width="600"><p>
+<form action="mul.php" method="POST">
+    <p>Введите число n: <input type="number" name="n" /></p>
+    <p>Введите число x: <input type="number" name="x" /></p>
+    <input type="submit" value="Вычислить сумму">
+</form>
+</script>
+</body>
+</html>
